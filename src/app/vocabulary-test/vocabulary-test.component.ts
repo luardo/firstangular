873caedd5b2a5a247348f0f2772a98ac7816138d
Luis Eduardo Rivera <luardohm@gmail.com>
2019-06-23 00:12:59 +0200

    add vocabulary test form component

--- src/app/vocabulary-test/vocabulary-test.component.ts
+++ src/app/vocabulary-test/vocabulary-test.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
-@Component({
-  selector: 'app-vocabulary-test',
-  templateUrl: './vocabulary-test.component.html',
-  styleUrls: ['./vocabulary-test.component.scss']
-})
+@Component({selector: "app-vocabulary-test", templateUrl: "./vocabulary-test.component.html", styleUrls: ["./vocabulary-test.component.scss"]})
 export class VocabularyTestComponent implements OnInit {
+  answerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder : FormBuilder) {}
 
   ngOnInit() {
+    this.answerForm = this.formBuilder.group({
+      answer: ["", Validators.required]
+    });
   }
-
 }
